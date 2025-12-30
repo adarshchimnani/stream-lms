85c11f523f63e8459fa16aa679c6d58279d17f8d
adarsh <adarsh.chimnani@gmail.com>
2025-12-30 23:46:17 +0100

    Student - SignUp

---
+++ actions/auth.ts
@@ -0,0 +1,30 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "../utils/supabase/server";
+
+export async function studentSignUp(formData: FormData) {
+    const supabase = await createClient();
+
+    //👇🏻 Extract form data
+    const credentials = {
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+        interest: formData.get("interest") as string,
+        name: formData.get("name") as string,
+    };
+
+    //👇🏻 Supabase sign up function (options attribute :- for user metadata)
+    const { data, error } = await supabase.auth.signUp({
+        email: credentials.email,
+        password: credentials.password,
+        options: {
+            data: {
+                interest: credentials.interest,
+                name: credentials.name,
+            },
+        },
+    });
+
+    //👉🏻 return user or error object
+}
