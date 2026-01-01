f7928379eef0d836d29958192c9fe2af980b771e
adarsh <adarsh.chimnani@gmail.com>
2026-01-01 23:37:16 +0100

    auth.ts - getUserSession

--- actions/auth.ts
+++ actions/auth.ts
@@ -181,3 +181,16 @@ export async function instructorLogIn(formData: FormData) {
 	revalidatePath("/", "layout");
 	return { error: null, status: 200, user: data.user };
 }
+
+export async function getUserSession() {
+	const supabase = await createClient();
+
+	const { data, error } = await supabase.auth.getUser();
+	if (error) {
+		return { error: error.message, status: error.status, user: null };
+	}
+	if (!data) {
+		return { error: "No session found", status: 404, user: null };
+	}
+	return { error: null, status: 200, user: data.user };
+}
