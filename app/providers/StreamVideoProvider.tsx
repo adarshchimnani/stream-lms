596edcd8d267e4c4da46dd49546ac924433b1158
adarsh <adarsh.chimnani@gmail.com>
2026-01-02 22:13:09 +0100

    StreamVideoProvider Component

---
+++ app/providers/StreamVideoProvider.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { createClient } from "@/utils/supabase/client";
+import { tokenProvider } from "@/actions/stream.action";
+import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
+import { useState, ReactNode, useEffect, useCallback } from "react";
+import { Loader2 } from "lucide-react";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY!;
+
+export const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+    const [videoClient, setVideoClient] = useState<StreamVideoClient | null>(
+        null
+    );
+    const supabase = createClient();
+
+    const getUser = useCallback(async () => {
+        //👉🏻 get user object from Supabase
+        //👉🏻 set Stream user data
+        // 👉🏻 initialize Stream video client using the Stream API key, Stream user data, and token Provider
+        
+    
+    }, [supabase.auth]);
+
+    useEffect(() => {
+        getUser();
+    }, [getUser]);
+
+    if (!videoClient)
+        return (
+            <div className='h-screen flex items-center justify-center'>
+                <Loader2 size='32' className='mx-auto animate-spin' />
+            </div>
+        );
+
+    return <StreamVideo client={videoClient}>{children}</StreamVideo>;
+};
